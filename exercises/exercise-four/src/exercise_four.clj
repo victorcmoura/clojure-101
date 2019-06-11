7fa8d5bc0e9aab7dee77e41de1fbe2571d5a34ec
Victor Moura <victor_cmoura@hotmail.com>
2019-06-11 14:26:36 -0300

    Add exercise four

---
+++ exercises/exercise-four/src/exercise_four.clj
@@ -0,0 +1,14 @@
+(ns exercise-four)
+
+(defn read-float[]
+    (let [readnumber (Double/parseDouble (read-line))] readnumber))
+
+(defn calculate-increase[salary, increase]
+    (let [result (+ salary (* salary (/ increase 100)))] result))
+
+(defn -main[]
+    (println "Insert the current salary")
+    (def salary (read-float))
+    (println "Insert the increase (percentage value)")
+    (def increase (read-float))
+    (println (calculate-increase salary increase)))
