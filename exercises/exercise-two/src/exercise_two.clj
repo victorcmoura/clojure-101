c459d17af4769d2f4184efbbdb6916d51674c26a
Victor Moura <victor_cmoura@hotmail.com>
2019-06-11 14:01:37 -0300

    Add exercise-two

---
+++ exercises/exercise-two/src/exercise_two.clj
@@ -0,0 +1,8 @@
+(ns exercise-two)
+
+(defn read-float[]
+    (let [readnumber (Double/parseDouble (read-line))] readnumber))
+
+(defn -main[]
+    (println "Insert three numbers")
+    (println (/ (+ (read-float) (+ (read-float) (read-float))) 3.0)))
